8eb236be8e4a5ca8e07052d88bca96e405d39f06
martin <martin.anlauf@onlinehome.de>
2021-01-08 22:38:25 +0100

    Added test cases

---
+++ src/test/kotlin/com/maranin/kotlinfundemo/v4io/V4ControllerTest.kt
@@ -0,0 +1,76 @@
+package com.maranin.kotlinfundemo.v4io
+
+import com.maranin.kotlinfundemo.shared.BaseController
+import com.maranin.kotlinfundemo.shared.Invoice
+import com.maranin.kotlinfundemo.shared.InvoiceError
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import java.time.LocalDate
+
+@SpringBootTest
+internal class V4ControllerTest {
+
+    @Autowired
+    lateinit var baseController: BaseController
+
+    @Autowired
+    lateinit var controller: V4Controller
+
+
+    private val date = LocalDate.of(2020, 11, 9)
+    private val dateString = "2020-11-09"
+
+    @BeforeEach
+    fun clearEntries() {
+        baseController.deleteEfforts()
+    }
+
+    @Test
+    fun recordEffort() {
+        val (d, numberOfHours) = baseController.recordEffort(dateString, 2)
+        expectThat(d).isEqualTo(date)
+        expectThat(numberOfHours).isEqualTo(2)
+    }
+
+    @Nested
+    inner class GetInvoice {
+
+        @Test
+        fun getInvoiceForKnownDay() {
+            baseController.recordEffort(dateString, 2)
+            val invoiceObject = controller.getInvoiceForDay(dateString)
+            expectThat(invoiceObject).isA<Invoice>()
+            val invoice = invoiceObject as Invoice
+            expectThat(invoice.from).isEqualTo(date)
+            expectThat(invoice.to).isEqualTo(date)
+            expectThat(invoice.hours).isEqualTo(2)
+            expectThat(invoice.hourlyWage).isEqualTo(5)
+            expectThat(invoice.amount).isEqualTo(11.6)
+        }
+
+        @Test
+        fun getInvoiceFornegativeHours() {
+            baseController.recordEffort(dateString, -2)
+            val invoiceObject = controller.getInvoiceForDay(dateString)
+            expectThat(invoiceObject).isA<InvoiceError>()
+            val error = invoiceObject as InvoiceError
+            expectThat(error.exception).isEqualTo("InvalidEntryException")
+        }
+
+        @Test
+        fun getInvoiceForUnknownDay() {
+            val invoiceObject = controller.getInvoiceForDay(dateString)
+            expectThat(invoiceObject).isA<InvoiceError>()
+            val error = invoiceObject as InvoiceError
+            expectThat(error.exception).isEqualTo("NoEntryAvailableException")
+        }
+
+    }
+
+}
